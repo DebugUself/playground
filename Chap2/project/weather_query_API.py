48ddfde7eaffb0e6f144ee2a3e89f6dfb8829068
birong ni <574168029@qq.com>
2017-08-22 12:41:01 +0800

    add w2d2 log;add api & different dir import recored

--- Chap2/project/weather_query_API.py
+++ Chap2/project/weather_query_API.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0,"/Users/NBR-hugh/Documents/github.nibirong.com/Py101-004/Chap1/project") # 让 python 解释器搜索该目录
+
 import weather_query
 
-weather_query.help()
+weather_query.main()
