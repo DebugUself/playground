7620cfa4909656e81614d70ddba6e7419839cf57
birong ni <574168029@qq.com>
2017-09-30 23:46:31 +0800

    restart

--- Chap3/project/WeatherQueryWebAPP_Flask.py
+++ Chap3/project/WeatherQueryWebAPP_Flask.py
@@ -7,7 +7,7 @@ Author: NBR-hugh
 Github: https://github.com/NBR-hugh/
 Edition：v1.0
 Edit date: 2017.09.13
-"""
+"""   
 
 from flask import Flask  #引入 flask 类
 app = Flask(__name__)  #该类的一个示例就是WSGI交互程序
@@ -16,4 +16,7 @@ app = Flask(__name__)  #该类的一个示例就是WSGI交互程序
 def hello_world() :  #函数名用于生成url,并返回想要呈现在用户浏览器的信息
     return "Hello,World!"
 
+@app. route('/helpinfo')
+def help_func()
+	return "help info"
 
