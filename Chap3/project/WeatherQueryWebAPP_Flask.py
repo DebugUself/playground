52e14be8cbd67dec67394fe25e83e7bc3dd8bef7
birong ni <574168029@qq.com>
2018-02-07 22:48:43 +0800

    function :query weather now   OK

--- Chap3/project/WeatherQueryWebAPP_Flask.py
+++ Chap3/project/WeatherQueryWebAPP_Flask.py
@@ -12,23 +12,54 @@ Edit date: 2017.09.13
 from flask import Flask # 引入 flask 类
 from flask import render_template # 引入渲染模板
 from flask import request
+from .API_deal import get_API_now
 
 app = Flask(__name__)  #该类的一个示例就是WSGI交互程序
+
+class ClassName(object):
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+
 
 
 @app.route('/',methods=["POST","GET"])  #告诉 flask 什么样的 url 可以触发函数
 def index_func(DisplayInfo=None) :  #函数名用于生成url,并返回想要呈现在用户浏览器的信息
-# 功能1:帮助
+    unit = "c"
+    DisplayInfo="欢迎使用~"
+
+    # 功能1:帮助
     if 'help' in request.form.keys():
-        help_info="""
-        Tips:
-            - 输入要查询的城市名称,回车确认将返回即时天气信息;
-            - 点击"help"按钮,显示帮助信息;
-            - 点击" history"按钮,显示查询历史. 
-        """
-        return render_template('Index.html',DisplayInfo=help_info)
+        help_info=help_func()
+        display_info=help_info
+
+    # 功能2:查询
+    elif 'query' in request.form.keys():
+        city =request.form['input_text']
+        API_return =get_API_now(unit, city)
+        weather_info =API_return[0]
+        print(history_list)
+        display_info =weather_info
+
+    elif 'history' in request.form.keys():
+        history_info =history_list
+        display_info =history_info
+
     else:
-        return render_template('Index.html',DisplayInfo="A Good Day! :D")
+        display_info= "Have A Good Day! :D"
+
+    history_list.append(weather_info)
+    return render_template('Index.html',DisplayInfo=display_info)
+
+def help_func():
+    help_info= """
+    帮助信息:
+        - 输入要查询的城市名称,回车确认将返回即时天气信息;
+        - 点击"help"按钮,显示帮助信息;
+        - 点击" history"按钮,显示查询历史. 
+               """
+    return help_info
 
 
 if __name__ == '__main__':
---
+++ Chap3/project/utils/const_value.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Program name: weather_query_API
+Author: NBR-hugh
+Github: https://github.com/NBR-hugh/
+Edition：v1.0
+Edit date: 2017.08.23
+"""
+
+# 知心天气
+KEY = "ozqwsdskrj99euhd"
+UID = "U72560B447"
+
+API_NOW = 'https://api.seniverse.com/v3/weather/now.json'
+API_DAILY = 'https://api.seniverse.com/v3/weather/daily.json'
+
+LANGUAGE = 'zh-Hans'
+
+START = 0
+DAYS = 3
+
+
+
+# OpenWeatherMap
+
+OWM_API = " https://api.openweathermap.org/data/2.5/weather"
+OWM_ID = "524901"
+OWM_APIKEY = "d966f1a05ff5652f1ee544dec9fb7bd0"
