a012ffeeea8b8cc48de0ed13fb3e300944f0492e
birong ni <574168029@qq.com>
2018-09-11 16:09:40 +0800

    database select succeed

--- Chap4/projects/untitled.py
+++ Chap4/projects/untitled.py
@@ -44,7 +44,7 @@ class SeniverseWeatherAPI():
         # self.params_daily={}
 
     def get_response(self):
-        print("Begin now weather query...")
+        print(">>> Begin now weather query...")
         response = requests.get(self.url, params=self.params_now, timeout=20)
         return response
 
@@ -71,15 +71,16 @@ class SeniverseWeatherAPI():
             {'status': 'The location can not be found.',
             'status_code': 'AP010010'}
         """
+        print(">>> Begin judge_response")
         response = self.get_response()
 
         if response.status_code == 200:
-            print('心知天气 API 请求成功!')
+            print('>>> 心知天气 API 请求成功!')
             weather_info = response.json()
             api_info = weather_info
 
         else:
-            print('心知天气 API 请求失败!')
+            print('>>> 心知天气 API 请求失败!')
             error_info = response.json()
             api_info = error_info
 
@@ -89,7 +90,9 @@ class SeniverseWeatherAPI():
         """Pick out now city weather.
         ref: https://www.seniverse.com/doc#now
         """
-        print(api_info)
+        print(">>>Begin pick_weather_now")
+
+        print(">>>The API Return:", api_info)
         city = api_info['results'][0]['location']['name']
         date = api_info['results'][0]['last_update']
         weather_now = api_info['results'][0]['now']
@@ -112,6 +115,7 @@ class WeatherDatabase():
         self.temp = weather_now['temperature']
         self.code = weather_now['code']
 
+        # 创建一个 table
         self.table_name = 'city_weather_now'
         self.create_table = '''
                             CREATE TABLE IF NOT EXISTS
@@ -122,6 +126,7 @@ class WeatherDatabase():
                                                 code tinyint)
                             '''
 
+        # 加入一行的天气信息
         self.row = (self.date, self.city,
                     self.weather_type, self.temp, self.code)
         self.insert_row = '''
@@ -129,27 +134,53 @@ class WeatherDatabase():
                             VALUES(?,?,?,?,?)
                           '''
 
-    def singl_operation(self, sql_commands, variable):
-        """Execute a single database(SQLite) opeantion
+        # 根据城市获取天气信息
+        self.select_rows = '''
+                            SELECT date, city, type, temp , code
+                            FROM city_weather_now
+                            WHERE city = ? 
+                            '''
+
+    def single_operation_to_db(self, sql_commands, variable):
+        """Execute a single  opeantion to database(SQLite): add,delete,update
         sql_comnads: a str,a single SQL statement(without ; )
         variable: a truple,table name/ clomuns name/ a insert raw..
         """
-        print(sql_commands)
+        print(">>>SQL 命令:", sql_commands)
         conn = sqlite3.connect('weather.db')
         c = conn.cursor()
         c.execute(sql_commands, variable)
+
+        if 'SELECT' in sql_commands:
+            select_result = c.fetchall()
+            print(""">>>  IF 显示选取内容:""", select_result)
+            return select_result
+
         conn.commit()
         conn.close()
+        print('>>> 数据库单次操作成功!')
 
     def insert_a_row_weather(self):
+        print(">>> 开始 insert 天气信息")
         sql_commands = self.insert_row
         variable = self.row
-        self.singl_operation(sql_commands, variable)
+        self.single_operation_to_db(sql_commands, variable)
         print(">> insert ok !")
+
+    def select_by_city(self, city):
+        print(">>> 开始 select 天气信息")
+
+        sql_commands = self.select_rows
+        variable = (city,)
+        print("选取城市:", city)
+        select_result =  self.single_operation_to_db(sql_commands, variable)
+        print(">> select ok !")
+
+        return select_result
 
 
 if __name__ == '__main__':
-    api = SeniverseWeatherAPI("四川", "unit")
+    api = SeniverseWeatherAPI("重庆", "unit")
     api_info = api.judge_response()
     print(api_info)
     date, city, weather_now = api.pick_weather_now(api_info)
@@ -157,3 +188,5 @@ if __name__ == '__main__':
 
     db = WeatherDatabase(date, city, weather_now)
     db.insert_a_row_weather()
+    select_result = db.select_by_city('重庆')
+    print(">>> Select 返回结果:",select_result)
